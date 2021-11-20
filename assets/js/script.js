eaf72b4b8c03585de6f26c6ffd6c36e4efafec44
VishrutMargam <vishrutm@axioned.com>
2021-11-20 14:39:38 +0530

    fixed js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,52 @@
-var dots = document.querySelectorAll('.dot');
-var navBtns = document.querySelectorAll('.navBtn');
-var slides = document.querySelectorAll(".mySlides");
+var dots = document.querySelectorAll('.slider-dots li a');
+var imageList = document.querySelectorAll('.slider li');
+var prevBtn = document.querySelector('.prev');
+var nextBtn = document.querySelector('.next');
 
-var slideIndex = 1;
-showSlides(slideIndex);
+let current = 0;
 
-// Next/previous controls
-navBtns.forEach((nav) => {
-    if(nav.classList.contains('prev')) {
-        nav.addEventListener('click', e => plusSlides(e, -1))
-    } else {
-        nav.addEventListener('click', e => plusSlides(e, 1))
-    }
-})
-
-function plusSlides(e, n) {
-    e.preventDefault();
-    showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-dots.forEach((dot, index) => {
-    dot.addEventListener('click', e => currentSlide(e, index+1))
-})
-
-function currentSlide(e ,n) {
-    e.preventDefault()
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var i;
-
-    if (n > slides.length) slideIndex = 1
-    if (n < 1) slideIndex = slides.length
-
-    for (i = 0; i < slides.length; i++) {
-        slides[i].className = 'mySlides fade none';
-    }
-
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-
-    slides[slideIndex-1].className = 'mySlides fade block'
-    dots[slideIndex-1].className += " active";
+function nextPicture(e) {
+  e.preventDefault();
+  const slideLength = imageList.length;
+  if (current < slideLength - 1) {
+    goTo(1);
   }
+}
+
+function prevPicture(e) {
+  e.preventDefault();
+  if (current > 0) {
+    goTo(-1);
+  }
+}
+
+function goTo(n) {
+  imageList.forEach((image) => {
+    if (image.classList.contains('active')) {
+      image.classList.remove('active');
+    }
+  });
+  imageList[current + n].classList.add('active');
+  current += n;
+}
+
+function changePicture(e, dot) {
+  e.preventDefault();
+
+  dots.forEach((dot) => dot.classList.remove('active'));
+  dot.classList.add('active');
+
+  imageList.forEach((image) => {
+    if (dot.dataset.tab == image.id) {
+      imageList.forEach((image) => image.classList.remove('active'));
+      image.classList.add('active');
+    }
+  });
+}
+
+nextBtn.addEventListener('click', nextPicture);
+prevBtn.addEventListener('click', prevPicture);
+
+dots.forEach((dot) => {
+  dot.addEventListener('click', (e) => changePicture(e, dot));
+});
